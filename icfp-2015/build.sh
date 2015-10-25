d0d3c394983690d4404cf50f51973d43e419b779
Tomas Petricek <tomas@tomasp.net>
2015-10-25 18:01:23 +0000

    Camera ready version of ML2014 post-proceedings

--- icfp-2015/build.sh
+++ icfp-2015/build.sh
@@ -1,3 +1,26 @@
-latex paper.tex --non-interactive
-bibtex paper
-pdflatex paper.tex
+#!/bin/bash
+if test "$OS" = "Windows_NT"
+then
+  MONO=""
+else
+  # Mono fix for https://github.com/fsharp/FAKE/issues/805
+  export MONO_MANAGED_WATCHER=false
+  MONO="mono"
+fi
+
+$MONO .paket/paket.bootstrapper.exe
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  exit $exit_code
+fi
+if [ -e "paket.lock" ]
+then
+  $MONO .paket/paket.exe restore
+else
+  $MONO .paket/paket.exe install
+fi
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  exit $exit_code
+fi
+$MONO packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
